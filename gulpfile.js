94dafdbb2486ec637f412300acd4354a47d27146
gabrielcorado <gabrielcorado@mail.com>
2015-09-21 16:15:17 -0300

    Different ways to compile the SDK

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,57 @@ var gulp = require('gulp'),
     rollup = require('rollup'),
     file = require('gulp-file');
 
+// Bundle UMD
 function bundle(format) {
   return rollup.rollup({
     entry: 'index.js',
-    external: ['cacheJS']
+    external: [
+      'rsvp',
+      'cacheJS',
+      'lodash/lang/isEmpty',
+      'lodash/lang/isArray',
+      'lodash/object/assign'
+    ]
   }).then(function(bundle) {
     return bundle.generate({
       format: format,
-      moduleName: 'yebo_sdk'
+      // sourceMap: true,
+      moduleName: 'YeboSDK'
     });
   });
 }
 
-function bundleTests(format) {
-  return rollup.rollup({
-    entry: 'tests/index.js',
-    external: ['cacheJS']
-  }).then(function(bundle) {
-    return bundle.generate({
-      format: format,
-      moduleName: 'yebo_sdk'
-    });
-  });
-}
+// gulp.task('build', function(done){
+//   return bundle('umd').then(function(gen) {
+//     return file('yebo_sdk.js', gen.code, {src: true})
+//       // .pipe(sourcemaps.init({loadMaps: true}))
+//       // .pipe(inlineEnv())
+//       .pipe(babel())
+//       // .pipe(sourcemaps.write('./'))
+//       .pipe(gulp.dest('dist'));
+//   });
+// })
 
 // Build the library
 gulp.task('build', function(done) {
-  bundle('umd').then(function(gen) {
-    file('yebo_sdk.js', gen.code, { src: true })
-      .pipe(babel())
-      .pipe(addsrc.prepend('./external/*.js'))
-      .pipe(concat('yebo_sdk.js'))
-      .pipe(gulp.dest('dist'))
-      .on('error', done)
-      .on('end', done);
-  }).catch(done);
+  gulp.src(['lib/**/*.js', 'yebo_sdk.js'])
+  // gulp.src('lib/yebo_sdk/request.js')
+  // gulp.src('index.js')
+    // .pipe(browserify({
+    //   transform: [
+    //     babelify.configure({
+    //       modules: 'umd'
+    //     })
+    //   ]
+    // }))
+    .pipe(babel({
+      modules: 'umd'
+    }))
+    .pipe(addsrc.prepend('./external/*.js'))
+    .pipe(concat('yebo_sdk.js'))
+    .pipe(gulp.dest('dist'))
+    .on('error', done)
+    .on('end', done);
 });
 
 // Build tests
