395b2e654a0e63e16e5cb231bd130b5ee71fa273
gabrielcorado <gabrielcorado@mail.com>
2015-10-20 10:04:47 -0200

    Move core classes to core/ folder

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //
-export { Request } from './lib/yebo_sdk/request';
-export { Query } from './lib/yebo_sdk/query';
-export { Store } from './lib/yebo_sdk/store';
-export { Config } from './lib/yebo_sdk/config';
+export { Request } from './lib/yebo_sdk/core/request';
+export { Query } from './lib/yebo_sdk/core/query';
+export { Store } from './lib/yebo_sdk/core/store';
+export { Config } from './lib/yebo_sdk/core/config';
 
 // Queries
 export { Products } from './lib/yebo_sdk/query/products';
