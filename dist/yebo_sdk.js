3d4b9eec780ce7d84e33f88ee660400d515d4c1e
yurifrl <yurifrl@hotmail.com>
2016-08-04 20:30:20 -0300

    migrate from order to order_token

--- dist/yebo_sdk.js
+++ dist/yebo_sdk.js
@@ -1614,7 +1614,7 @@ var User = (function () {
       };
 
       // Check if the orderToken is defined
-      if (orderToken !== undefined) options['order'] = orderToken;
+      if (orderToken !== undefined) options['order_token'] = orderToken;
 
       // Return a promise
       return new _rsvp2['default'].Promise(function (resolve, reject) {
