82a590b9583caf9fc098c874953491c3989c91d7
gabrielcorado <gabrielcorado@mail.com>
2016-08-12 11:57:59 -0300

    Adding a library entrypoint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Yebo SDK
+ * This library is the bridge between the Yebo E-commerce API
+ * and JavaScript applications
+ *
+ * @author <Yebo E-commerce>
+ * @contributors <Gabriel Corado, Gabriela Caldeira Diogo>
+ */
+
+// Example of export
+// export products from './products';
