18c749a94bea141509ab1c3cdcb788be3316f04e
gabrielcorado <gabrielcorado@mail.com>
2016-08-12 14:28:38 -0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-/**
- * Yebo SDK
- * This library is the bridge between the Yebo E-commerce API
- * and JavaScript applications
- *
- * @author <Yebo E-commerce>
- * @contributors <Gabriel Corado, Gabriela Caldeira Diogo>
- */
-
 // Example of export
-// export products from './products';
+// export * from './products/index';
+// export default {};
