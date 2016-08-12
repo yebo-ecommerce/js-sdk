0ecd54b1fd293e11e243b6feab55da2a6c8bf18e
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-12 17:40:29 -0300

    Src modification

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // Example of export
 // export * from './products/index';
 // export default {};
+// Yebo - yebo
+
+// yebo.modules.getProduct();
+// yebo.api.fetch('');
+// yebo.auth.getCredentials();
+
+// import {getProducts} from 'yebo';
