eba2ea32e4417b43131fc8337917f1d80437d3d8
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-15 18:16:17 -0300

    Orders

---
+++ test/modules/orders/index.spec.js
@@ -0,0 +1,18 @@
+// Helper
+const h = require('./../../helpers');
+
+// Include the product module
+import * as m from 'src/modules/orders';
+
+// Products Module!
+describe('Products module', () => {
+  // getOrders
+  it('should return all a user Orders', () => {
+    // ...
+  });
+
+  // getOrdersNumber
+  it('should return a user Order', () => {
+    // ...
+  });
+});
