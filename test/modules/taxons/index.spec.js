48893d4b63f94d6d9a18342a21931a44e2b756a4
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-15 18:16:36 -0300

    Users

---
+++ test/modules/taxons/index.spec.js
@@ -0,0 +1,28 @@
+// Helper
+const h = require('./../../helpers');
+
+// Include the product module
+import * as m from 'src/modules/taxons';
+
+// Products Module!
+describe('Products module', () => {
+  // getTaxons
+  it('should return taxons', () => {
+    // ...
+  });
+
+  // getTaxonId
+  it('should return a single taxon', () => {
+    // ...
+  });
+
+  // getTaxonomies
+  it('should return taxonomies', () => {
+    // ...
+  });
+
+  // getTaxonomyId
+  it('should return a single taxonomy', () => {
+    // ...
+  });
+});
