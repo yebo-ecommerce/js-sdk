7f4ef1fa4dc1f55faa546fadcd9e4ca5cbff4f80
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-15 18:15:58 -0300

    Cart

---
+++ test/modules/cart/index.spec.js
@@ -0,0 +1,38 @@
+// Helper
+const h = require('./../../helpers');
+
+// Include the product module
+import * as m from 'src/modules/orders';
+
+// Products Module!
+describe('Products module', () => {
+  // createCart
+  it('should return validation cart', () => {
+    // ...
+  });
+
+  // getCartItems
+  it('should return all items from the cart', () => {
+    // ...
+  });
+
+  // emptyCartItems
+  it('should return all items from the cart', () => {
+    // ...
+  });
+
+  // addCartItems
+  it('should return all items cart', () => {
+    // ...
+  });
+
+  // removeCartItems
+  it('should return all items cart', () => {
+    // ...
+  });
+
+  // updateCartItems
+  it('should return all items cart', () => {
+    // ...
+  });
+});
