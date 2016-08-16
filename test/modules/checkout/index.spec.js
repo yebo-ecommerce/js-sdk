ed0875223deaa70e90d97973c9cb86e9c31e793a
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-16 16:55:21 -0300

    Checkout test

---
+++ test/modules/checkout/index.spec.js
@@ -0,0 +1,48 @@
+// Helper
+const h = require('./../../helpers');
+
+// Include the product module
+import * as m from 'src/modules/checkout';
+
+// Products Module!
+describe('Products module', () => {
+  // getOrderAddress
+  it('should return a address', () => {
+    // ...
+  });
+
+  // createOrderAddress
+  it('should return a Create the address of the Order', () => {
+    // ...
+  });
+
+  // removeOrderAddress
+  it('should return a Remove the address of the Order', () => {
+    // ...
+  });
+
+  // updateOrderAddress
+  it('should return a Update the address of the Order', () => {
+    // ...
+  });
+
+  // getOrderShipments
+  it('should return a delivery methods for Order', () => {
+    // ...
+  });
+
+  // setOrderShipment
+  it('should return a delivery method for a package', () => {
+    // ...
+  });
+
+  // getOrderPay
+  it('should return a payment methods for Order', () => {
+    // ...
+  });
+
+  // orderPay
+  it('should return a Making the payment', () => {
+    // ...
+  });
+});
