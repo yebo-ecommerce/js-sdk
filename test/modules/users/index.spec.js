9677a2638c061a7cb60ba25bc442941eab1a2634
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-08-16 16:56:35 -0300

    Change user test

---
+++ test/modules/users/index.spec.js
@@ -0,0 +1,28 @@
+// Helper
+const h = require('./../../helpers');
+
+// Include the product module
+import * as m from 'src/modules/users';
+
+// Products Module!
+describe('Products module', () => {
+  // registerUser
+  it('should return a User Registration', () => {
+    // ...
+  });
+
+  // loginUser
+  it('should return a login user', () => {
+    // ...
+  });
+
+  // resetUser
+  it('should return a password change', () => {
+    // ...
+  });
+
+  // resetUserReset
+  it('should return a new password', () => {
+    // ...
+  });
+});
