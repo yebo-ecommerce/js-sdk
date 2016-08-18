1224122e31c99ef5f031fa223d505670f1abf07a
gabrielcorado <gabrielcorado@mail.com>
2016-08-18 10:32:19 -0300

    Chaging method `fetch`

--- src/core/request.js
+++ src/core/request.js
@@ -7,18 +7,25 @@
 
 // Dependencies
 import axios from 'axios';
-// import assing from 'lodash';
 
 /**
  * General purpose fetch, could be used by anyone
+ *
+ * @param {String} method The HTTP method (mainly 'GET' and 'POST')
+ * @param {url} url The URL that will be fetched
+ * @param {Object} data Data that will be sent to the URL
+ * @param {String} contentType The type of data
+ * @param {Object} headers The HTTP request headers
+ * @return {Promise} A promise
  */
-export const fetch = function(method, url, data, token, options) {
+export const fetch = function(method, url, data, contentType, headers) {
   // Create the request
   return axios({
     url: url,
     method: method,
-    contentType: '',
+    contentType: contentType,
     crossOrigin: true,
+    headers: headers,
     data:data
   });
 }
