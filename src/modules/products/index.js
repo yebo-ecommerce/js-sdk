1a4339bc68bcf2cefaf236b54649fdb05243b02f
gabrielcorado <gabrielcorado@mail.com>
2016-08-18 10:48:24 -0300

    Example of `api` usage

--- src/modules/products/index.js
+++ src/modules/products/index.js
@@ -1,10 +1,17 @@
+// Dependencies
+import {buildRequest, executeRequest} from './../../core/api'
+
 /**
  * List of Products
  * @param {Object} search Parameters used in the products search
  * @return
  */
 export const getProducts = function (search) {
-  // ...
+  // Build the request.
+  let req = buildRequest('GET', '/products', search);
+
+  // Return the execution
+  return excuteRequest(req);
 }
 
 /**
