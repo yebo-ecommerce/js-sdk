eb3bae84f04a950599eaf3d622cc20272d941363
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-09-02 12:03:00 -0300

    Change for test

--- test/modules/cart/filters.spec.js
+++
@@ -1,9 +0,0 @@
-// Include the index module
-import * as m from 'src/modules/cart/index';
-
-// Module Cart
-describe('modules cart index', () => {
-  it('create carts', () {
-    let createcarts()
-  })
-});
