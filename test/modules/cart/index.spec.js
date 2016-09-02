470491966bd5f7b06c367d1a045e11d8d6af48e3
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-09-02 14:18:51 -0300

    Index test

--- test/modules/cart/index.spec.js
+++ test/modules/cart/index.spec.js
@@ -8,17 +8,41 @@ import * as m from 'src/modules/cart';
 describe('Cart module', () => {
   // createCart
   it('should return valid cart', () => {
-    // ...
+    //
+      let testcarts = m.createCart('token', '10', true, false, 'user_token')
+    //
+      h.expect(testcarts).to.have.property('token');
+      h.expect(testcarts).to.have.property('number');
+      h.expect(testcarts).to.have.property('last');
+      h.expect(testcarts).to.have.property('create');
+      h.expect(testcarts).to.have.property('user_token');
+    });
   });
 
   // getCartItems
   it('should return all items from the cart', () => {
-    // ...
+    //
+      let returnitem = m.cartItems('tokens', '15', false, true, 'usertoken')
+    //
+      h.expect(returnitem).to.have.property('token');
+      h.expect(returnitem).to.have.property('number');
+      h.expect(returnitem).to.have.property('last');
+      h.expect(returnitem).to.have.property('create');
+      h.expect(returnitem).to.have.property('userToken');
+    });
   });
 
   // emptyCartItems
   it('should return all items from the cart', () => {
-    // ...
+    //
+      let cartitem = m.cart_item('tokens', '54', false, true, 'usertoken')
+    //
+      h.expect(cartitem).to.have.property('token');
+      h.expect(cartitem).to.have.property('number');
+      h.expect(cartitem).to.have.property('last');
+      h.expect(cartitem).to.have.property('create');
+      h.expect(cartitem).to.have.property('userToken');
+    });
   });
 
   // addCartItems
