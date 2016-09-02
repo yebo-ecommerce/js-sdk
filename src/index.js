26c196ee0a14890d1f455d62b6fb74d125c9908c
gabrielcorado <gabrielcorado@mail.com>
2016-09-02 14:47:26 -0300

    Exporting!!!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-// Example of export
-// export * from './products/index';
-// export default {};
-// Yebo - yebo
+/**
+ * Yebo SDK Main
+ */
 
-// yebo.modules.getProduct();
-// yebo.api.fetch('');
-// yebo.auth.getCredentials();
+// Exporting core functionalities
+export * from './core/api';
+export * from './core/request';
+export * from './core/config';
 
-// import {getProducts} from 'yebo';
+// Exporting the modules
+export * from './modules/cart/index';
+export * from './modules/checkout/index';
+export * from './modules/orders/index';
+export * from './modules/products/index';
+export * from './modules/taxons/index';
+export * from './modules/users/index';
+
+// SDK version
+export const version = '1.0.0-beta';
