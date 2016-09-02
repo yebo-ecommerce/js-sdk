29091b24d003ca86d923c0f01538a6103e29d585
Gabriela Diogo <gabriela.cdiogo@hotmail.com>
2016-09-02 15:33:47 -0300

    Change Test in Cart

--- src/modules/cart/index.js
+++ src/modules/cart/index.js
@@ -56,8 +56,8 @@ export const emptyCartItems = function (token, number, last, create, userToken) 
  * @param {Boolean} last If an order is not found, search for the last not complete
  * @param {Boolean} create If you find an Order (even the last incomplete) initialize a new
  * @param {String} userToken User token to validate the Order
- * @param {Integer} variant Variant ID to be added
- * @param {Integer} qty Quantity of products to be added
+ * @param {Number} variant Variant ID to be added
+ * @param {Number} qty Quantity of products to be added
  * @return
  */
 export const addCartItems = function (token, number, last, create, userToken, variant, qty) {
--- test/modules/cart/index.spec.js
+++ test/modules/cart/index.spec.js
@@ -9,7 +9,7 @@ describe('Cart module', () => {
   // createCart
   it('should return valid cart', () => {
     //
-      let testcarts = m.createCart('token', '10', true, false, 'user_token')
+      let testcarts = m.createCart('token', 10, true, false, 'user_token')
     //
       h.expect(testcarts).to.have.property('token');
       h.expect(testcarts).to.have.property('number');
@@ -17,12 +17,11 @@ describe('Cart module', () => {
       h.expect(testcarts).to.have.property('create');
       h.expect(testcarts).to.have.property('user_token');
     });
-  });
 
   // getCartItems
   it('should return all items from the cart', () => {
     //
-      let returnitem = m.cartItems('tokens', '15', false, true, 'usertoken')
+      let returnitem = m.cartItems('tokens', 15, false, true, 'usertoken')
     //
       h.expect(returnitem).to.have.property('token');
       h.expect(returnitem).to.have.property('number');
@@ -30,12 +29,11 @@ describe('Cart module', () => {
       h.expect(returnitem).to.have.property('create');
       h.expect(returnitem).to.have.property('userToken');
     });
-  });
 
   // emptyCartItems
   it('should return all items from the cart', () => {
     //
-      let cartitem = m.cart_item('tokens', '54', false, true, 'usertoken')
+      let cartitem = m.cart_item('tokens', 54, false, true, 'usertoken')
     //
       h.expect(cartitem).to.have.property('token');
       h.expect(cartitem).to.have.property('number');
@@ -43,11 +41,19 @@ describe('Cart module', () => {
       h.expect(cartitem).to.have.property('create');
       h.expect(cartitem).to.have.property('userToken');
     });
-  });
 
   // addCartItems
   it('should return all items cart', () => {
-    // ...
+    //
+      let addItem = m.addCartItems('tokens', 54, false, true, 'usertoken', 32, 54)
+      //
+      h.expect(addItem).to.have.property('token');
+      h.expect(addItem).to.have.property('number');
+      h.expect(addItem).to.have.property('last');
+      h.expect(addItem).to.have.property('create');
+      h.expect(addItem).to.have.property('userToken');
+      h.expect(addItem).to.have.property('variant');
+      h.expect(addItem).to.have.property('qty');
   });
 
   // removeCartItems
