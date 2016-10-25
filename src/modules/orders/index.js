62e59f3aa896d89464b945b43a288953417b8fbd
Gabriel Corado <gabrielcorado@mail.com>
2016-10-25 16:52:55 -0200

    Moving orders module to the new model (build/execute)

--- src/modules/orders/index.js
+++ src/modules/orders/index.js
@@ -1,3 +1,20 @@
+// Dependencies
+import { buildRequest } from './../../core/api'
+
+/**
+ * Returns all a user Orders
+ * Same params as `getOrders`
+ * @return {Object} Request
+ */
+export const buildGetOrders = function (token, complete, incomplete, page = 1, perPage = 25) {
+  //
+  if (complete === true && incomplete === true)
+    throw 'A order could not be complete and incomplete at same time.'
+
+  // Build the request.
+  return buildRequest('GET', `/orders`, { token: token, complete: complete, incomplete: incomplete, page: page, per_page: perPage  });
+}
+
 /**
  * Returns all a user Orders
  * @param {String} token Token user, owner of Orders
@@ -6,15 +23,25 @@
  * @return
  */
 export const getOrders = function (token, complete, incomplete, page = 1, perPage = 25) {
+  // Build the request.
+  let req = buildGetOrder(token, complete, incomplete, page, perPage);
+
+  // Return the execution
+  return excuteRequest(req);
+}
+
+/**
+ * Returns a user Order
+ * Same params as `getOrderNumber`
+ * @return {Object} Request
+ */
+export const buildGetOrderNumber = function (token, number, complete = true, incomplete = false) {
   //
   if (complete === true && incomplete === true)
     throw 'A order could not be complete and incomplete at same time.'
 
   // Build the request.
-  let req = buildRequest('GET', `/orders`, { token: token, complete: complete, incomplete: incomplete, page: page, per_page: perPage  });
-
-  // Return the execution
-  return excuteRequest(req);
+  return buildRequest('GET', `/orders/${number}`, { token: token, complete: complete, incomplete: incomplete });
 }
 
 /**
@@ -25,13 +52,9 @@ export const getOrders = function (token, complete, incomplete, page = 1, perPag
  * @param {Boolean} incomplete Returns only if the Order is incomplete
  * @return
  */
-export const getOrderNumber = function (token, number, complete = false, incomplete = false) {
-  //
-  if (complete === true && incomplete === true)
-    throw 'A order could not be complete and incomplete at same time.'
-
+export const getOrderNumber = function (token, number, complete = true, incomplete = false) {
   // Build the request.
-  let req = buildRequest('GET', `/orders/${number}`, { token: token, complete: complete, incomplete: incomplete });
+  let req = buildGetOrderNumber(token, number, complete, incomplete);
 
   // Return the execution
   return excuteRequest(req);
